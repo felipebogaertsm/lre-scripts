e76319659becdc10412d6e9e1f80c271bae12a9b
Felipe Bogaerts de Mattos <me@felipebm.com>
2025-02-15 12:21:59 -0300

    Increased precision of print statements

--- src/main.py
+++ src/main.py
@@ -41,10 +41,10 @@ ox_flow = oxidizer_flow_from_of_ratio(
     of_ratio=OF_RATIO, total_flow=mass_flow_total
 )  # kg/s
 
-print(f"Total mass flow: {mass_flow_total:.3f} kg/s")
-print(f"Fuel flow: {fuel_flow:.3f} kg/s")
-print(f"Oxidizer flow: {ox_flow:.3f} kg/s")
-print(f"Oxidizer to fuel ratio: {ox_flow / fuel_flow:.3f}")
+print(f"Total mass flow: {mass_flow_total:.4f} kg/s")
+print(f"Fuel flow: {fuel_flow:.4f} kg/s")
+print(f"Oxidizer flow: {ox_flow:.4f} kg/s")
+print(f"Oxidizer to fuel ratio: {ox_flow / fuel_flow:.4f}")
 
 # STEP 2
 CHAMBER_TEMP = 3445  # Kelvin
@@ -52,15 +52,15 @@ gas_temp_at_throat = temperature_at_throat(
     chamber_temperature=CHAMBER_TEMP, gamma=GAMMA
 )  # Kelvin
 
-print(f"Chamber temperature: {CHAMBER_TEMP:.3f} K")
-print(f"Gas temperature at throat: {gas_temp_at_throat:.3f} K")
+print(f"Chamber temperature: {CHAMBER_TEMP:.4f} K")
+print(f"Gas temperature at throat: {gas_temp_at_throat:.4f} K")
 
 # STEP 3
 pressure_at_throat = pressure_at_throat(
     chamber_pressure=CHAMBER_PRESSURE, gamma=GAMMA
 )  # Pascals
 
-print(f"Pressure at throat: {pressure_at_throat * 1e-6:.3f} MPa")
+print(f"Pressure at throat: {pressure_at_throat * 1e-6:.4f} MPa")
 
 # STEP 4
 throat_area = throat_area_from_mass_flow(
@@ -71,12 +71,12 @@ throat_area = throat_area_from_mass_flow(
     molar_mass=MOLAR_MASS_PROPELLANT,
 )  # m^2
 
-print(f"Throat area: {throat_area * 1e4:.3f} cm^2")
+print(f"Throat area: {throat_area * 1e4:.4f} cm^2")
 
 # STEP 5
 throat_diameter = area_to_diameter(area=throat_area)  # m
 
-print(f"Throat diameter: {throat_diameter * 1e3:.3f} mm")
+print(f"Throat diameter: {throat_diameter * 1e3:.4f} mm")
 
 # STEP 6
 optimal_er = optimal_expansion_ratio(
@@ -86,17 +86,17 @@ optimal_er = optimal_expansion_ratio(
 )
 exit_area = throat_area * optimal_er  # m^2
 
-print(f"Optimal expansion ratio: {optimal_er:.3f}")
-print(f"Exit area: {exit_area * 1e4:.3f} cm^2")
+print(f"Optimal expansion ratio: {optimal_er:.4f}")
+print(f"Exit area: {exit_area * 1e4:.4f} cm^2")
 
 # STEP 7
 exit_diameter = area_to_diameter(exit_area)  # m
-print(f"Exit diameter: {exit_diameter * 1e3:.3f} mm")
+print(f"Exit diameter: {exit_diameter * 1e3:.4f} mm")
 
 # STEP 8
 chamber_volume = L_STAR * throat_area  # m^3
 
-print(f"Chamber volume: {chamber_volume * 1e6:.3f} L")
+print(f"Chamber volume: {chamber_volume * 1e6:.4f} L")
 
 # STEP 9
 chamber_diameter = 5 * throat_diameter  # m
@@ -104,15 +104,15 @@ chamber_length = chamber_volume / (
     diameter_to_area(diameter=chamber_diameter) * 1.1
 )  # m
 
-print(f"Chamber diameter: {chamber_diameter * 1e3:.3f} mm")
-print(f"Chamber length: {chamber_length * 1e3:.3f} mm")
+print(f"Chamber diameter: {chamber_diameter * 1e3:.4f} mm")
+print(f"Chamber length: {chamber_length * 1e3:.4f} mm")
 
 # STEP 10
 chamber_wall_thickness = (CHAMBER_PRESSURE * chamber_diameter) / (
     2 * CHAMBER_YIELD_STRENGTH / SAFETY_FACTOR
 )
 
-print(f"Chamber wall thickness: {chamber_wall_thickness * 1e3:.3f} mm")
+print(f"Chamber wall thickness: {chamber_wall_thickness * 1e3:.4f} mm")
 
 # STEP 14
 fuel_injector_area = fuel_spray_injector_area(
@@ -122,7 +122,7 @@ fuel_injector_area = fuel_spray_injector_area(
     pressure_drop=FUEL_PRESSURE_DROP,
 )
 
-print(f"Fuel injector area: {fuel_injector_area * 1e6:.3f} mm^2")
+print(f"Fuel injector area: {fuel_injector_area * 1e6:.4f} mm^2")
 
 # STEP 15
 gas = ct.Solution("air.yaml")  # Load Cantera's air model
@@ -133,6 +133,6 @@ gas.X = {OX_NAME: 1.0} # pure O₂ as the only component using mole fractions
 ox_density_at_entrance = gas.density
 ox_injector_area = ox_flow / (ox_density_at_entrance * OX_VELOCITY)  # m^2
 
-print(f"Oxidizer pressure at entrance: {(CHAMBER_PRESSURE + OX_PRESSURE_DROP) * 1e-6:.3f} MPa")
-print(f"Oxidizer density at entrance: {ox_density_at_entrance:.3f} kg/m^3")
-print(f"Oxidizer injector area: {ox_injector_area * 1e6:.3f} mm^2")
+print(f"Oxidizer pressure at entrance: {(CHAMBER_PRESSURE + OX_PRESSURE_DROP) * 1e-6:.4f} MPa")
+print(f"Oxidizer density at entrance: {ox_density_at_entrance:.4f} kg/m^3")
+print(f"Oxidizer injector area: {ox_injector_area * 1e6:.4f} mm^2")
